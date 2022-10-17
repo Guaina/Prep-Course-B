98153081ecbd22e529f8263e5a0e748f7323a697
Farah <hermfarah@gmail.com>
2022-10-17 15:35:46 -0300

    HWJSIII

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,54 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
-}
+    var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    var suma = numeros.reduce(function (acc, elem) {
+     return acc + elem;
+    })
+    return suma;
+   }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var pares = [];
+  for (i = 0; i < array.length; i++) {
+    var par = array[i] % 2;
+    if (par === 0){
+      pares.push(array[i]);
+    }
+  } return pares;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  var alCuadrado = []
+  for (i = 0; i < array.length; i++){
+    var potencia = array[i] ** 2;
+    alCuadrado.push(potencia);
+  }
+  return alCuadrado;
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var resultado = 0
+  for (i = 0; i < array.length; i++){
+    var resultado = resultado + array[i];
+  }
+  return resultado;
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  var digitos = num.toString()
+  return digitos.length;
 }
   
   
